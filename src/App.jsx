d348d634a68210fe647b57ad7e5c7e1b57127235
jtschams <jtschams@yahoo.com>
2024-07-21 01:05:18 -0500

    added logo and css.  created navbar. removed extraneous code from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navigation from './components/Navigation'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,1 +1,33 @@
-// TODO: Navbar
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  const [page, setPage] = useState(location.pathname)
+
+  const links = [
+    {
+      text: 'About Me',
+      link: '/',
+    }, {
+      text: 'Portfolio',
+      link: '/portfolio',
+    }, {
+      text: 'Contact Me',
+      link: '/contact',
+    }, {
+      text: 'Resume',
+      link: '/resume',
+    }
+  ]
+  return (
+    <nav>
+      <ul>
+        {links.map((link) =>
+          (<li key={link.link}><Link to={link.link} className={page === link.link ? 'active' : null}  onClick={() => setPage(link.link)}>{link.text}</Link></li>)
+        )}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation;
