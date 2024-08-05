fcbfd243ee91ad2b7a407401d070d86cee665169
jtschams <jtschams@yahoo.com>
2024-08-04 19:36:50 -0500

    refined portfolio visuals

--- client/src/components/SingleRepo.jsx
+++ client/src/components/SingleRepo.jsx
@@ -1,13 +1,27 @@
 export default function SingleRepo(project) {
+  // return (
+  //   <article className='project-card'>
+  //     <img src={project.preview} />
+  //     <div className='repo-info'>
+  //       <h3>{project.name}</h3>
+  //       <div>
+  //         <a href={project.page} target='_blank' rel="noopener noreferrer">Website</a>
+  //         <a href={project.repo} target='_blank' rel="noopener noreferrer">Github</a>
+  //       </div>
+  //     </div>
+  //   </article>
+  // )
   return (
     <article className='project-card'>
       <img src={project.preview} />
       <div className='repo-info'>
-        <h3>{project.name}</h3>
-        <div>
-          <a href={project.page} target='_blank' rel="noopener noreferrer">Website</a>
-          <a href={project.repo} target='_blank' rel="noopener noreferrer">Github</a>
-        </div>
+        <hgroup>
+          <h3>{project.name}</h3>
+          <div>
+            <a href={project.page} target='_blank' rel="noopener noreferrer">Website</a>
+            <a href={project.repo} target='_blank' rel="noopener noreferrer">Github</a>
+          </div>
+        </hgroup>
       </div>
     </article>
   )
