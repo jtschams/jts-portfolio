efd782bc78824f6a4c4bbb60d5b7f8e9a730d579
jtschams <jtschams@yahoo.com>
2024-08-20 14:01:22 -0500

    added new picture, tweaked about me

--- client/src/pages/AboutMe.jsx
+++ client/src/pages/AboutMe.jsx
@@ -1,5 +1,4 @@
 import SelfPicture from '../assets/me.jpg';
-// TODO: Better picture
 
 export default function AboutMe() {
 
@@ -8,7 +7,7 @@ export default function AboutMe() {
       <header><h2>About Me</h2></header>
       <section>
         <img className='selfPicture' src={SelfPicture} alt='A picture of JT Schams'></img>
-        <p>Hello!  My name is JT Schams.  I'm from College Station and recently finished up a Full Stack Development course.  I have 12 years of experience in the medical office field.  During my time there, I participated in the transition to and use of 3 different Electronic Health Record systems.  As a result, I have a strong understanding of the end user's perspective.<br/><br/>I'm now looking to start a new career that better suits my abilities and education.  I can code Full Stack applications, and I particularly excel in backend development.  I have experience with React on the front end, and with MongoDB, PostgreSQL, and Node.js on the backend.</p>
+        <p>Hello!  My name is JT Schams.  I'm from College Station, Texas, and I recently finished up a Full Stack Development course.  I have 12 years of experience in a medical office.  During my time there, I participated in the transition from paper records to an Electronic Health Record system.  Over the years, the office changed software 3 times.  As a result, I have a strong understanding of the end user's perspective.<br/><br/>I'm now looking to start a new career that better suits my abilities and education.  I can code Full Stack applications, and I particularly excel in backend development.  I have experience with React on the front end, and with MongoDB, PostgreSQL, and Node.js on the backend.</p>
       </section>
     </main>
   )
